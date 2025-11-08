a85bf25eb8caa6102773b1ceac5a3b55a15e244c
k1 <keyvan.arjmnd@gmail.com>
2025-11-08 08:03:31 -0800

    migrations

---
+++ Oprim.Domain/Migrations/20251108151800_nullable calender.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oprim.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class nullablecalender : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkTemplates_ProjectCalendar_CalendarId",
+                table: "WorkTemplates");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CalendarId",
+                table: "WorkTemplates",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkTemplates_ProjectCalendar_CalendarId",
+                table: "WorkTemplates",
+                column: "CalendarId",
+                principalTable: "ProjectCalendar",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkTemplates_ProjectCalendar_CalendarId",
+                table: "WorkTemplates");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CalendarId",
+                table: "WorkTemplates",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkTemplates_ProjectCalendar_CalendarId",
+                table: "WorkTemplates",
+                column: "CalendarId",
+                principalTable: "ProjectCalendar",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Oprim.Domain/Migrations/20251108153442_nullabe role.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oprim.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class nullaberole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkTemplateArticles_OrganizationRole_RoleId",
+                table: "WorkTemplateArticles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkTemplateArticles_Stakeholder_StakeholderId",
+                table: "WorkTemplateArticles");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "StakeholderId",
+                table: "WorkTemplateArticles",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RoleId",
+                table: "WorkTemplateArticles",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkTemplateArticles_OrganizationRole_RoleId",
+                table: "WorkTemplateArticles",
+                column: "RoleId",
+                principalTable: "OrganizationRole",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkTemplateArticles_Stakeholder_StakeholderId",
+                table: "WorkTemplateArticles",
+                column: "StakeholderId",
+                principalTable: "Stakeholder",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkTemplateArticles_OrganizationRole_RoleId",
+                table: "WorkTemplateArticles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkTemplateArticles_Stakeholder_StakeholderId",
+                table: "WorkTemplateArticles");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "StakeholderId",
+                table: "WorkTemplateArticles",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "RoleId",
+                table: "WorkTemplateArticles",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkTemplateArticles_OrganizationRole_RoleId",
+                table: "WorkTemplateArticles",
+                column: "RoleId",
+                principalTable: "OrganizationRole",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkTemplateArticles_Stakeholder_StakeholderId",
+                table: "WorkTemplateArticles",
+                column: "StakeholderId",
+                principalTable: "Stakeholder",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
